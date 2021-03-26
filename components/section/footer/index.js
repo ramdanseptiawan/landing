2a8da7c9c3a9cd8aab4d498981e38ee61e4de897
azharizkita <azharizkita@icloud.com>
2021-03-26 18:01:44 +0700

    new feature section on jembatanku

--- components/section/footer/index.js
+++ components/section/footer/index.js
@@ -22,7 +22,7 @@ const Footer = () => {
                                 className={'grid grid-cols-1 md:grid-cols-3 gap-8 px-8 md:px-0'}
                             >
                                 <figure>
-                                    <svg fill={'#F7F7F2'} className={'mx-auto w-24 h-24'} focusable="false" viewBox="0 0 24 24" aria-hidden="true" tabIndex="-1" title="PhoneIphone" data-ga-event-category="material-icons" data-ga-event-action="click" data-ga-event-label="PhoneIphone"><path d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"></path></svg>
+                                    <svg fill={'#4285F4'} className={'mx-auto w-24 h-24'} focusable="false" viewBox="0 0 24 24" aria-hidden="true" tabIndex="-1" title="PhoneIphone" data-ga-event-category="material-icons" data-ga-event-action="click" data-ga-event-label="PhoneIphone"><path d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"></path></svg>
                                     <div className={'mx-auto text-lg font-semibold text-center text-gray-200'}>
                                         <a href={'tel:+62-22-4268728'}>
                                             Phone: +62-22-4268728
@@ -30,7 +30,7 @@ const Footer = () => {
                                     </div>
                                 </figure>
                                 <figure>
-                                    <svg fill={'#F7F7F2'} className={'mx-auto w-24 h-24'} focusable="false" viewBox="0 0 24 24" aria-hidden="true" tabIndex="-1" title="LocationOn" data-ga-event-category="material-icons" data-ga-event-action="click" data-ga-event-label="LocationOn"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"></path></svg>
+                                    <svg fill={'#DB4437'} className={'mx-auto w-24 h-24'} focusable="false" viewBox="0 0 24 24" aria-hidden="true" tabIndex="-1" title="LocationOn" data-ga-event-category="material-icons" data-ga-event-action="click" data-ga-event-label="LocationOn"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"></path></svg>
                                     <div className={'mx-auto text-lg font-semibold text-center text-gray-200'}>
                                         <a href={'https://g.page/langgeng-cipta-solusi?gm'}>
                                             Jl. Cihampelas No. 2 Bandung, 40116, Jawa Barat, Indonesia
@@ -38,7 +38,7 @@ const Footer = () => {
                                     </div>
                                 </figure>
                                 <figure>
-                                    <svg fill={'#F7F7F2'} className={'mx-auto w-24 h-24'} focusable="false" viewBox="0 0 24 24" aria-hidden="true" tabIndex="-1" title="Email" data-ga-event-category="material-icons" data-ga-event-action="click" data-ga-event-label="Email"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path></svg>
+                                    <svg fill={'#F4B400'} className={'mx-auto w-24 h-24'} focusable="false" viewBox="0 0 24 24" aria-hidden="true" tabIndex="-1" title="Email" data-ga-event-category="material-icons" data-ga-event-action="click" data-ga-event-label="Email"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path></svg>
                                     <div className={'mx-auto text-lg font-semibold text-center text-gray-200'}>
                                         <a href={'mailto:cs@langgengciptasolusi.com'}>
                                             cs@langgengciptasolusi.com
--- pages/jembatanku/index.js
+++ pages/jembatanku/index.js
@@ -4,14 +4,19 @@ import LCSLogo from '../../assets/Logo/lcs.svg';
 import JembatankuLogo from '../../assets/Logo/jembatanku.svg';
 import { InView } from 'react-intersection-observer';
 import JembatankuBG from './jembatanku_bg.jpeg';
+import Audit from './Audit.png';
+import Historical from './Historical.png';
 
 import dynamic from 'next/dynamic';
 import Router from 'next/router';
-const Topology = dynamic(() => import('./Topology.svg'));
+const Centralized = dynamic(() => import('./Centralized.svg'));
+const Sync = dynamic(() => import('./Sync.svg'));
+const Infographic = dynamic(() => import('./Infographic.svg'));
 const Documents = dynamic(() => import('../../components/section/documents'));
 const Footer = dynamic(() => import('../../components/section/footer'));
 
 const Navbar = ({ section = 'landing' }) => {
+    console.log(section)
     return <nav
         style={{ zIndex: 166782760, background: '#121113', height: 72 }}
         className={'fixed w-full gap-12 hidden md:flex overflow-x-auto flex-row align-middle transform py-4 px-12 ' + (section !== 'landing' ? 'shadow-xl' : 'opacity-90')}
@@ -29,6 +34,12 @@ const Navbar = ({ section = 'landing' }) => {
             Topology
 			<div className={'pt-2 pb-1'}>
                 <div className={'opacity-95 bg-gray-100 rounded-full ' + (section === 'topology' ? 'h-1 w-full' : '')} />
+            </div>
+        </a>
+        <a href={'#features'} className={'m-auto text-gray-100 font-semibold ' + (section === 'features' ? 'font-extrabold' : '')}>
+            Features
+			<div className={'pt-2 pb-1'}>
+                <div className={'opacity-95 bg-gray-100 rounded-full ' + (section === 'features' ? 'h-1 w-full' : '')} />
             </div>
         </a>
         <a href={'#documents'} className={'m-auto text-gray-100 font-semibold ' + (section === 'documents' ? 'font-extrabold' : '')}>
@@ -118,21 +129,132 @@ export default function Home() {
                 >
                     <section
                         id={'topology'}
-                        style={{ background: '#222725' }}
+                        className={'bg-gray-200'}
                     >
                         <div className={'container m-auto py-12 min-h-screen h-full relative grid grid-cols-2 gap-6 content-center'}>
                             <div className={'col-span-2 h-full z-50 p-6 md:col-span-1 flex flex-wrap content-center items-center'}>
                                 <div className={'mx-auto space-y-6'}>
-                                    <h2 className="text-5xl text-center md:text-left leading-none font-extrabold text-gray-100 tracking-tight mb-8">
+                                    <h2 className="text-5xl text-center md:text-left leading-none font-extrabold tracking-tight mb-8">
                                         Talking about topology.
 									</h2>
-                                    <p className={'text-gray-200 text-lg text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                    <p className={'text-lg text-justify font-medium sm:leading-10 mx-auto mb-6'}>
                                         We designed the "client first" topology, in terms to deliver the most up-to-date data synchronization. Using this topology, we eliminate so much delay between every updates.
                                     </p>
                                 </div>
                             </div>
                             <div className={'z-40 p-6 w-full col-span-2 md:col-span-1 flex flex-wrap content-center items-center m-auto'}>
-                                <Topology className={'m-auto flex-grow'} />
+                                <Infographic className={'m-auto flex-grow'} />
+                            </div>
+                        </div>
+                    </section>
+                </InView>
+                <InView
+                    threshold={0.1}
+                    onChange={(inView) => {
+                        if (inView) {
+                            setSection('features')
+                            console.log('features')
+                        }
+                    }}
+                >
+                    <section
+                        id={'features'}
+                    >
+                        <div className={'container m-auto py-12 min-h-screen h-full relative grid grid-cols-2 gap-6 content-center'}>
+                            <div className={'col-span-2 h-full z-50 p-6 md:col-span-full flex flex-wrap content-center items-center'}>
+                                <div className={'mx-auto space-y-6'}>
+                                    <h2 className="text-5xl text-center leading-none font-extrabold tracking-tight mb-8">
+                                        Features
+									</h2>
+                                    <p className={'text-lg text-center font-medium sm:leading-10 mx-auto mb-6'}>
+                                        We deliver the best possible feature to our beloved JembatanKu.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className={'z-40 p-6 w-full col-span-full md:col-span-1 flex flex-wrap content-center items-center m-auto'}>
+                                <Centralized className={'m-auto flex-grow p-10'} />
+                            </div>
+                            <div className={'col-span-full h-full z-50 p-6 md:col-span-1 flex flex-wrap content-center items-center'}>
+                                <div className={'mx-auto space-y-2'}>
+                                    <h2 className="text-4xl text-center leading-none font-extrabold tracking-tight mb-8">
+                                        Integrated and Centralized System
+									</h2>
+                                    <p className={'text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                        Our goal is to implement the one-for-many concept. JembatanKu can handle as many bridge as client wants in one single screen, it is also smartly simplified every bridge informations.
+                                    </p>
+                                    {/* <p className={'text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                        We also implement the centralized database concept so our clients don't need a seperated application just to handle a specific bridge.
+                                    </p> */}
+                                </div>
+                            </div>
+                            <div className={'col-span-full h-full z-50 p-6 md:col-span-1 flex flex-wrap content-center items-center'}>
+                                <div className={'mx-auto space-y-6'}>
+                                    <h2 className="text-4xl text-center leading-none font-extrabold tracking-tight mb-8">
+                                        Fully Realtime
+									</h2>
+                                    <p className={'text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                        No matter whenever and wherever you are, every updates done to the database will automatically changes the global application state, right away.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className={'z-40 p-6 w-full col-span-full md:col-span-1 flex flex-wrap content-center items-center m-auto'}>
+                                <Sync className={'m-auto flex-grow p-6'} />
+                            </div>
+                            <div className={'z-40 p-6 w-full col-span-full md:col-span-1 flex flex-wrap content-center items-center m-auto'}>
+                                <img className={'m-auto flex-grow rounded-xl shadow-lg'} src={Audit} />
+                            </div>
+                            <div className={'col-span-full h-full z-50 p-6 md:col-span-1 flex flex-wrap content-center items-center'}>
+                                <div className={'mx-auto space-y-6'}>
+                                    <h2 className="text-4xl text-center leading-none font-extrabold tracking-tight mb-8">
+                                        Tracable Activities
+									</h2>
+                                    <p className={'text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                        When talking about security, we actively improve the security layer so clients don't need to worry about suspicious activities. So we include the audit system into JembatanKu.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className={'col-span-full h-full z-50 p-6 md:col-span-1 flex flex-wrap content-center items-center'}>
+                                <div className={'mx-auto space-y-6'}>
+                                    <h2 className="text-4xl text-center leading-none font-extrabold tracking-tight mb-8">
+                                        Tracable Events
+									</h2>
+                                    <p className={'text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                        Based on recieved alerts, privileged clients can immidiately deploy technical engineers into a specific bridge. Every alerts caused by sensors data anomaly are also recorded into the central database.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className={'z-40 p-6 w-full col-span-full md:col-span-1 flex flex-wrap content-center items-center m-auto'}>
+                                <img className={'m-auto flex-grow rounded-xl shadow-lg'} src={Historical} />
+                            </div>
+                            <div className={'z-40 p-6 w-full col-span-full md:col-span-1 flex flex-wrap content-center items-center m-auto'}>
+                                <video playsInline className={'z-20 m-auto flex-grow rounded-xl shadow-lg max-w-none min-w-full min-h-full'} autoPlay loop muted>
+                                    <source src={'/Realtime.mp4'} type="video/mp4" />
+                                </video>
+                            </div>
+                            <div className={'col-span-full h-full z-50 p-6 md:col-span-1 flex flex-wrap content-center items-center'}>
+                                <div className={'mx-auto space-y-6'}>
+                                    <h2 className="text-4xl text-center leading-none font-extrabold tracking-tight mb-8">
+                                        Realtime Sensor Updates
+									</h2>
+                                    <p className={'text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                        JembatanKu don't stop as an "early warning system" only. Clients can also take a look at the real sensor data using our data graph, and of course, in real time.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className={'col-span-full h-full z-50 p-6 md:col-span-1 flex flex-wrap content-center items-center'}>
+                                <div className={'mx-auto space-y-6'}>
+                                    <h2 className="text-4xl text-center leading-none font-extrabold tracking-tight mb-8">
+                                        Easy Threshold Control
+									</h2>
+                                    <p className={'text-justify font-medium sm:leading-10 mx-auto mb-6'}>
+                                        Who want's to change a threshold by coming into a specific bridge everyday? Right. Nobody does. For that purpose, we make a robust threshold changer. So clients can change any sensor threshold from anywhere and anytime. And yes, in real time.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className={'z-40 p-6 w-full col-span-full md:col-span-1 flex flex-wrap content-center items-center m-auto'}>
+                                <video playsInline className={'z-20 m-auto flex-grow rounded-xl shadow-lg max-w-none min-w-full min-h-full'} autoPlay loop muted>
+                                    <source src={'/Threshold.mp4'} type="video/mp4" />
+                                </video>
                             </div>
                         </div>
                     </section>
@@ -149,7 +271,7 @@ export default function Home() {
                         id={'documents'}
                     >
                         <div className={'container m-auto py-12 min-h-screen h-full relative grid grid-cols-3 gap-6 content-center'}>
-                            <div className={'col-span-3 md:col-span-1 h-full z-50 p-6 flex flex-wrap content-center items-center'}>
+                            <div className={'col-span-full h-full z-50 p-6 flex flex-wrap content-center items-center'}>
                                 <div className={'mx-auto space-y-6'}>
                                     <h2 className="text-5xl text-center md:text-left leading-none font-extrabold text-gray-900 tracking-tight mb-8">
                                         Documents.
@@ -159,10 +281,10 @@ export default function Home() {
                                     </p>
                                 </div>
                             </div>
-                            <div className={'col-span-3 md:col-span-2 z-40 p-6 w-full h-full flex flex-wrap content-center items-center m-auto'}>
+                            <div className={'col-span-full z-40 p-6 w-full h-full flex flex-wrap content-center items-center m-auto'}>
                                 <div className="relative flex min-w-full min-h-full">
                                     <div className={'absolute z-30 flex min-h-full min-w-full'}>
-                                        <div className={'flex-grow md:rounded-3xl transform shadow-lg bg-gradient-to-tr from-yellow-400 to-yellow-700  md:-rotate-2'}></div>
+                                        <div className={'flex-grow md:rounded-3xl transform shadow-lg bg-gradient-to-tr from-yellow-400 to-yellow-700  md:-rotate-1'}></div>
                                     </div>
                                     <Documents />
                                 </div>
